e131beef8b0f19072f64a4f2933e364a6e490fc6
jinseokYeom <jinyeom95@gmail.com>
2016-06-20 17:32:15 -0500

    progress

--- syntax/syntax.go
+++ syntax/syntax.go
@@ -1,3 +1,15 @@
 package syntax
 
 import ()
+
+// Syntactic Atom
+type SAtom struct {
+	atom     string
+	optional bool
+}
+
+// Syntactic rule
+type Syntax struct {
+	first *SAtom
+	next  []*SAtom
+}
